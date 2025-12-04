8b426157e671f817da977d86b1764d8febb29d5c
DeTiveNC <116792124+detivenc@users.noreply.github.com>
2025-12-04 21:08:26 +0100

    Change portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
-import { languages, defaultLang } from './src/i18n/utils';
-import { defineConfig } from 'astro/config';
+import { languages, defaultLang } from "./src/i18n/utils";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://detivenc.github.io',
-    i18n: {
-        locales: Object.keys(languages),
-        defaultLocale: defaultLang,
-        routing: {
-            prefixDefaultLocale: true
-        }
+  site: "https://detivenc.github.io",
+  i18n: {
+    locales: Object.keys(languages),
+    defaultLocale: defaultLang,
+    routing: {
+      prefixDefaultLocale: true,
     },
-    output: 'static'
+  },
+  output: "static",
 });
